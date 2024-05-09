1fd87686325312bfdda6b766809a4957ab04ce01
jpedro002 <joaopsilvavolei@gmail.com>
2024-05-09 15:40:56 -0300

    push

--- index.py
+++ index.py
@@ -1,12 +1,17 @@
 import os
 import random
 
-with open('shablau.txt', 'w') as f:
-    f.write(str(random.randint(1, 100)))
-
-os.system('git add shablau.txt')
-os.system('git commit -m "Adicionado número aleatório em shablau.txt"')
-os.system('git push')
-os.system('git checkout b1')
-os.system('git merge b2')
-
+while(True):
+    i =  input('digite *push* para fazer um push de tudo ou *mdpush* ou *parar*')
+    
+    if i == "parar": break
+    elif i == "push": 
+        os.system('git add .')
+        os.system('git commit -m "push"')
+        os.system('git push')
+    elif i == "mdpush":
+        with open('shablau.txt', 'w') as f:
+            f.write(str(random.randint(1, 100)))
+            os.system('git add shablau.txt')
+            os.system('git commit -m "Adicionado número aleatório em shablau.txt"')
+            os.system('git push')

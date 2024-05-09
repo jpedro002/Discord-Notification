eaa5938446c85250c72e36ccbacd849ca128f91e
jpedro002 <joaopsilvavolei@gmail.com>
2024-05-09 12:14:01 -0300

    sdk.fbasdf

--- discordNotification/src/main.ts
+++ discordNotification/src/main.ts
@@ -104,7 +104,7 @@ const sendDiscordMessage = async (
     await axios.post(webhook, personalizedEmbed);
   } else {
     const data = await fillDefaultEmbed();
-
+    core.info("Sending message to Discord");
     console.log(data);
     await axios.post(webhook, data);
   }
